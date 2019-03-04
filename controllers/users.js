84094f4df7d647533ce23b3402064cc6e8275e9d
zhoushugang <zhoushugang@itcast.cn>
2019-03-04 16:31:12 +0800

    黑马64期品优购 第六天代码  第二节课提交

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,40 @@
 //定义用户相关的路由函数
-
+const svgCaptcha = require('svg-captcha')
+const createError = require('http-errors')
 //页面展示
-exports.login = (req,res,next) =>{
+exports.login = (req, res, next) => {
+  //1. 通过 svg-captcha 的包  生产svg格式的图片   svg标签 HTML
+  const captcha = svgCaptcha.createMathExpr({width: 120, height: 30, fontSize: 30})
+  //2. captcha {data:svg格式图片,text:图片的内容或者公式结果}
+  res.locals.svg = captcha.data
+  req.session.text = captcha.text //记录结果  下次校验对比
   res.render('login.art')
 }
 //登录逻辑
-exports.loginLogic = (req,res,next) =>{
-  res.send('ok')
+exports.loginLogic = (req, res, next) => {
+  /*需求*/
+  const {username, password, captcha, auto} = req.body
+  Promise.resolve().then(() => {
+    /*1. 保证表单数据完整性  校验*/
+    if (!(username && password && captcha)) throw createError(456, '表单必须输入完整')
+    /*2. 验证码  校验*/
+    if (captcha !== req.session.text) throw  createError(456, '验证码错误')
+    /*3. 用户名和密码调接口  校验*/
+    /*4. 如果自动登录选中  准备自动登录的数据*/
+    /*5. 合并购物车*/
+  }).catch(err => {
+    /*6. 补充：错误提示统一处理*/
+    /*6.1 页面展示错误提示信息*/
+    /*6.2 自己创建的信息  程序运行时错误信息  456状态*/
+    if (err.status === 456) {
+      res.locals.msg = err.message
+    } else {
+      res.locals.msg = '登录失败'
+    }
+    /*6.3 重新生成验证码*/
+    const captcha = svgCaptcha.createMathExpr({width: 120, height: 30, fontSize: 30})
+    res.locals.svg = captcha.data
+    req.session.text = captcha.text //记录结果  下次校验对比
+    res.render('login.art')
+  })
 }
